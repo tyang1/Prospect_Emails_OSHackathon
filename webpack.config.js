719bd706255985191fff831231dd67aad91bad5b
Tiffany Yang <teefyang7857@gmail.com>
2020-09-09 11:47:33 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin =  require('html-webpack-plugin');
+
+module.exports = {
+    entry : './app/index.jsx',
+    output : {
+        path : path.resolve(__dirname , 'dist'),
+        filename: 'index_bundle.js'
+    },
+    module : {
+        rules : [
+            {test : /\.(jsx)$/, use:'babel-loader'},
+            {test : /\.css$/, use:['style-loader', 'css-loader']}
+        ]
+    },
+    mode:'development',
+    plugins : [
+        new HtmlWebpackPlugin ({
+            template : 'app/index.html'
+        })
+    ]
+
+}

8c85c41f6114dcf3848971f8b0023e1410e3b67a
Tiffany Yang <teefyang7857@gmail.com>
2020-09-09 14:13:12 -0700

    adds form

--- app/components/MyForm.jsx
+++ app/components/MyForm.jsx
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import Form from 'muicss/lib/react/form';
 import Input from 'muicss/lib/react/input';
 import Textarea from 'muicss/lib/react/textarea';
 import Button from 'muicss/lib/react/button';
 import { useForm } from "react-hook-form";
+// import { RootRef } from '@material-ui/core';
 
 function onSubmitForm(formData) {
-  alert("Hi your phone number is: " + formData.phoneNumber);
+    //where to call the API to invoke image creation
+  console.log(formData)
 }
-
-export default class MyForm extends React.Component {
-    render() {
+function showRef(data) {
+    console.log("showRef", data)
+}
+export default function MyForm() {
+      const { register, handleSubmit } = useForm();
       return (
-        <Form>
-          <legend>Title</legend>
-          <Input label="Required Text Field" required={true} />
-          <Input label="Required Email Address" type="email" floatingLabel={true} required={true} />
-          <Textarea label="Required Textarea" floatingLabel={true} required={true} />
-          <Input label="Email Address" type="email" defaultValue="Validation error" />
-          <Button variant="raised">Submit</Button>
+        <Form onSubmit = {handleSubmit(onSubmitForm)}>
+          <legend>Template Fields</legend>
+          <input name="email" ref={register} label="Email Address" type="email" required={true} />
+          <Textarea name="fullName" ref={register} label="Required User Name" floatingLabel={true} required={true} />
+          <input name="siteUrl" ref={register} label="Required Site URL" floatingLabel={true} required={true} />
+          <input name="backgroundColor" ref={register} label="Background Color" floatingLabel={true} required={true} />
+          <input name="brandLogo" ref={register} label="Brand Icon/Logo" floatingLabel={true} required={true} />
+          <Button variant="raised">Go Create Image!</Button>
         </Form>
       );
-    }
+    
   }
 
 // export default function MyForm() {

f53cd3ab70a10dce5ce8a5f205599838039b60c8
Tiffany Yang <teefyang7857@gmail.com>
2020-09-27 19:08:04 -0700

    fixes useForm hook to get formData

--- app/components/MyForm.jsx
+++ app/components/MyForm.jsx
@@ -1,30 +1,75 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Form from 'muicss/lib/react/form';
 import Input from 'muicss/lib/react/input';
 import Textarea from 'muicss/lib/react/textarea';
 import Button from 'muicss/lib/react/button';
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from 'react-hook-form';
 import { palette } from '@material-ui/system';
 
-
 export default function MyForm(props) {
-      // const { handleImageOutput } = props;
-      const { register, handleSubmit } = useForm();
-      const onSubmitForm = (formData) => {
-        console.log("inside fake handleImageOutput", formData)
-        // handleImageOutput(formData)
-      }
-      return (
-        <Form onSubmit = {handleSubmit(onSubmitForm)}>
-          <legend>Template Fields</legend>
-          <Input name="companyName" ref={register} label="Company" type="email" required={true} />
-          <Input name="siteUrl" ref={register} label="Required Site URL" floatingLabel={true} required={true} />
-          <Input name="backgroundColor" ref={register} label="Background Color" floatingLabel={true} required={true} />
-          <Input name="Icon" ref={register} label="Icon" floatingLabel={true} required={true} />
-          <Input name="websiteImage" ref={register} label="Website Image" floatingLabel={true} required={true} />
-          <Textarea name="notificationText" ref={register} label="Notification Text" floatingLabel={true} required={true} />
-          <Button variant="raised">Download Image!</Button>
-        </Form>
-      );
-    
-  }
+  // const { handleImageOutput } = props;
+  const { control, handleSubmit } = useForm();
+  const onSubmitForm = (formData) => {
+    console.log('inside fake handleImageOutput', formData);
+    // handleImageOutput(formData)
+  };
+  return (
+    <Form onSubmit={handleSubmit(onSubmitForm)}>
+      <legend>Template Fields</legend>
+      <Controller
+        as={Input}
+        name='companyName'
+        control={control}
+        defaultValue=''
+        label='Company'
+        required={true}
+      />
+      <Controller
+        as={Input}
+        name='siteUrl'
+        control={control}
+        defaultValue=''
+        label='Required Site URL'
+        floatingLabel={true}
+        required={true}
+      />
+      <Controller
+        as={Input}
+        name='backgroundColor'
+        control={control}
+        defaultValue=''
+        label='Background Color'
+        floatingLabel={true}
+        required={true}
+      />
+      <Controller
+        as={Input}
+        name='Icon'
+        control={control}
+        defaultValue=''
+        label='Icon'
+        floatingLabel={true}
+        required={true}
+      />
+      <Controller
+        as={Input}
+        name='websiteImage'
+        control={control}
+        defaultValue=''
+        label='Website Image'
+        floatingLabel={true}
+        required={true}
+      />
+      <Controller
+        as={Textarea}
+        name='notificationText'
+        control={control}
+        defaultValue=''
+        label='Notification Text'
+        floatingLabel={true}
+        required={true}
+      />
+      <Button variant='raised'>Download Image!</Button>
+    </Form>
+  );
+}

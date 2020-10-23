1c5a2126b6b90451cc253295b1d259e3845a0e18
Tiffany Yang <teefyang7857@gmail.com>
2020-10-22 23:06:00 -0700

    implements external input on PUSH_NOTIF_HOST and UEL_TEXT

--- server/reimage.js
+++ server/reimage.js
@@ -2,13 +2,13 @@ const { execFile, exec } = require('child_process');
 
 module.exports = function reimage(userInputs = null) {
   const HEADER_TEXT = 'Notifications for Saturday123';
-  const PUSH_NOTIF_HOST = 'bandolierstyle.com';
+  const PUSH_NOTIF_HOST = 'OneSignalStyle.com';
   const URL_TEXT = 'bandolierstyle.com';
 
   return new Promise((resolve, reject) => {
     execFile(
       `sh gen_image.sh`,
-      [`"${HEADER_TEXT}"`],
+      [`"${HEADER_TEXT}"`, `"${PUSH_NOTIF_HOST}"`, `"${URL_TEXT}"`],
       { cwd: process.cwd() + '/imgbuilder', shell: true },
       (err, stdout, stderr) => {
         if (err) {

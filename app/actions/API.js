c7e92d595211e8119e8cd206963788bf21647015
Tiffany Yang <teefyang7857@gmail.com>
2021-03-16 12:58:46 -0700

    refactors API.js

--- app/actions/API.js
+++ app/actions/API.js
@@ -1,37 +1,39 @@
-const axios = require('axios').default;
+const axios = require('axios').default
 
-export async function getImages(data, setImage) {
-  await axios({
-    url: `${process.env.DEV_API_URL}/images`,
-    method: 'post',
-    data,
-  })
-    .then((response) => {
-      let imageURL = `data:image/jpg;base64,${response.data}`;
-      setImage(imageURL);
-      return;
+export function getImages(data, setImage) {
+  return new Promise((resolve, reject) => {
+    axios({
+      url: `${process.env.DEV_API_URL}/images`,
+      method: 'post',
+      data,
     })
-    .catch((error) => {
-      console.log(error);
-    });
+      .then((response) => {
+        let imageURL = `data:image/jpg;base64,${response.data}`
+        resolve(setImage(imageURL))
+      })
+      .catch((error) => {
+        reject(new Error(error))
+      })
+  })
 }
 
-export async function downloadImage(data) {
-  await axios({
-    url: `${process.env.DEV_API_URL}/images`,
-    method: 'post',
-    data,
-  })
-    .then(async (response) => {
-      const link = document.createElement('a');
-      link.href = `data:image/jpg;base64,${response.data}`;
-      link.setAttribute('download', 'out.jpg'); //or any other extension
-      document.body.appendChild(link);
-      await link.click();
-      document.body.removeChild(link);
-      return;
+export function downloadImage(data) {
+  return new Promise((resolve, reject) => {
+    axios({
+      url: `${process.env.DEV_API_URL}/images`,
+      method: 'post',
+      data,
     })
-    .catch((error) => {
-      console.log(error);
-    });
+      .then(async (response) => {
+        const link = document.createElement('a')
+        link.href = `data:image/jpg;base64,${response.data}`
+        link.setAttribute('download', 'out.jpg') //or any other extension
+        document.body.appendChild(link)
+        await link.click()
+        resolve(document.body.removeChild(link))
+      })
+      .catch((error) => {
+        reject(new Error(error))
+      })
+  })
 }
